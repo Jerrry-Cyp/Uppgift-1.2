e19dc50fdea9a0b9b519a12603897a5f8bc062e3
alsa07007 <alsa07007@E-PW05CGR5>
2024-09-12 13:19:47 +0200

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Timers;
+using System.Threading;
+
+// Uppgift 1.2
+namespace Uppgift_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("An error has occurred. To continue:");
+            Console.WriteLine("Press Enter to return to Windows, or");
+            Console.WriteLine("Press CRTL+ALT+DEL to restart your computer.");
+            Console.ReadKey();
+        }
+    }
+}
